bb4dd60ee5193536e76b830106de69caf3537698
Miguel Corvo <mikelangelon@gmail.com>
2018-03-29 19:44:42 +0200

    Adding images

--- app/src/main/java/carta/fluxxy/com/cartamasalta/MainActivity.kt
+++ app/src/main/java/carta/fluxxy/com/cartamasalta/MainActivity.kt
@@ -1,7 +1,7 @@
 package carta.fluxxy.com.cartamasalta
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
