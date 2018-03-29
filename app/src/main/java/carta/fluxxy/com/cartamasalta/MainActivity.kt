b8865412c483de93b80a5185ca6539bfb6d7cb79
Miguel Corvo <mikelangelon@gmail.com>
2018-03-29 18:12:31 +0200

    First commit

---
+++ app/src/main/java/carta/fluxxy/com/cartamasalta/MainActivity.kt
@@ -0,0 +1,12 @@
+package carta.fluxxy.com.cartamasalta
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
